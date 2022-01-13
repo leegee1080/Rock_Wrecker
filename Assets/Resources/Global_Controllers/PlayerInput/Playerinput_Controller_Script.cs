51e2c8d4ff5dd4459c256ced79de4907f5a885d2
Mitchell Greene <mitchell.lee.greene@gmail.com>
2022-01-13 12:57:28 -0500

    more work on the player controls

--- Assets/Resources/Global_Controllers/PlayerInput/Playerinput_Controller_Script.cs
+++ Assets/Resources/Global_Controllers/PlayerInput/Playerinput_Controller_Script.cs
@@ -8,8 +8,10 @@ public class Playerinput_Controller_Script : MonoBehaviour
 {
     public static Playerinput_Controller_Script playerinput_controller_singleton;
 
-    public Event tap_down;
-    public Event tap_up;
+    private PlayerInputActions player_input_actions;
+
+    public Event tap_down_event;
+    public Event tap_up_event;
 
 
     private void Awake()
@@ -24,5 +26,23 @@ public class Playerinput_Controller_Script : MonoBehaviour
             return;
         }
         DontDestroyOnLoad(this);
+
+        player_input_actions = new PlayerInputActions();
+    }
+
+    private void Tap_Down(){
+
+    }
+
+    private void Tap_Up(){
+        
+    }
+
+    private void OnEnable(){
+        player_input_actions.Enable();
+    }
+
+    private void OnDisable(){
+        player_input_actions.Disable();
     }
 }

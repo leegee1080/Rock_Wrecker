6a5ebb444f7d603aae479d1072a48c8e667e454e
Mitchell Greene <mitchell.lee.greene@gmail.com>
2022-03-14 21:04:04 -0400

    started work on the enemy scripts

---
+++ Assets/Resources/Scripts/Levelplay_Scripts/EnemyScriptableObjects/AttackTypeClasses/EnemyAttack_Simple.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack_Simple : AttackTypeClass
+{
+    public override void Attack()
+    {
+        return;
+    }
+}
--- Assets/Resources/Scripts/Levelplay_Scripts/LevelEnemy_Script.cs
+++ Assets/Resources/Scripts/Levelplay_Scripts/LevelEnemy_Script.cs
@@ -2,73 +2,89 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
-public class LevelPlayer_Script : LevelActor_Script
+public enum EnemyTypes
 {
-    
-    public PlayerInputActions.PlayerControlsActions player_actions{get; private set;}
+    Normal,
+    Killable
+}
+
+interface Iattack
+{
+    public void Attack();
+}
+public abstract class AttackTypeClass: Iattack
+{
+    public abstract void Attack();
+}
+
+
+public class LevelEnemy_Script : LevelActor_Script
+{
     public float move_timer = 0;
-
-    private PlayerStatesAbstractClass _currentPlayerStateClass;
-
-    [SerializeField] ParticleSystem _pickUpRock_ps;
-    public Animator playerAnimator;
+    public float spawnTimer = 0;
+    private EnemyStatesAbstractClass _currentEnemyStateClass;
+    public Animator enemyAnimator;
 
 
 
     public override void Start()
     {
         base.Start();
-
-        player_actions = Playerinput_Controller_Script.playerinput_controller_singleton.player_input_actions.PlayerControls;
-        // Input_Control_Events.move_up_event += Moveup_Player;
-        // Input_Control_Events.move_down_event += Movedown_Player;
-        // Input_Control_Events.move_right_event += Moveright_Player;
-        // Input_Control_Events.move_left_event += Moveleft_Player;
     }
+
+    public void StartSpawn()//enemy type so)
+    {
+        //grab stats from SO
+        //grab animator from instanced GO
+        _currentEnemyStateClass = new EnemyState_Start();
+        _currentEnemyStateClass.OnEnterState(this);
+    }
+
+    
 
     public override Level_Actor_States_Enum Change_Level_Actor_State(Level_Actor_States_Enum new_state)
     {
 
-        if(_currentPlayerStateClass != null){ _currentPlayerStateClass.OnExitState(this);}
+        if(_currentEnemyStateClass != null){ _currentEnemyStateClass.OnExitState(this);}
         switch (new_state)
         {
             case Level_Actor_States_Enum.Normal:
-                _currentPlayerStateClass = new PlayerState_Normal();
+                _currentEnemyStateClass = new EnemyState_Normal();
                 break;
             case Level_Actor_States_Enum.Dead:
-                _currentPlayerStateClass = new PlayerState_Dead();
+                _currentEnemyStateClass = new EnemyState_Dead();
                 break;
             case Level_Actor_States_Enum.Frozen:
-                _currentPlayerStateClass = new PlayerState_Frozen();
+                _currentEnemyStateClass = new EnemyState_Frozen();
                 break;
             case Level_Actor_States_Enum.Pause:
-                _currentPlayerStateClass = new PlayerState_Pause();
+                _currentEnemyStateClass = new EnemyState_Pause();
                 break;
             case Level_Actor_States_Enum.Moving:
-                _currentPlayerStateClass = new PlayerState_Moving();
+                _currentEnemyStateClass = new EnemyState_Moving();
                 break;
             default:
                 return current_state;
         }
         last_state = current_state;
         current_state = new_state;
-        _currentPlayerStateClass.OnEnterState(this);
+        _currentEnemyStateClass.OnEnterState(this);
         return current_state;
     }
 
     private void Update()
     {
-        _currentPlayerStateClass.OnUpdateState(this);
+        _currentEnemyStateClass.OnUpdateState(this);
     }
+
+
 
     public void Move(int Direction)
     {
         Vector2Int desired_coord = Vector2Int.zero;
         switch (Direction)
         {
-             case (int)Actor_Direction_Enum.up:
+            case (int)Actor_Direction_Enum.up:
                 RotateActor(Vector3.zero);
                 desired_coord = grid_pos + new Vector2Int(0,1);
                 break;
@@ -93,18 +109,14 @@ public class LevelPlayer_Script : LevelActor_Script
         if(Levelplay_Controller_Script.levelplay_controller_singleton.x_lead_map_coord_array[desired_coord.x][desired_coord.y].resident != null && Levelplay_Controller_Script.levelplay_controller_singleton.x_lead_map_coord_array[desired_coord.x][desired_coord.y].resident.moveable)
         {
             move_timer = 0.25f;
-            Check_For_Exit_Tile(desired_coord);
-            playerAnimator.SetTrigger("Melee Attack");
-            _pickUpRock_ps.Play();
-            SwapResidentsTweened(this, Levelplay_Controller_Script.levelplay_controller_singleton.x_lead_map_coord_array[desired_coord.x][desired_coord.y].resident, 1f);
-            Change_Level_Actor_State(Level_Actor_States_Enum.Moving);
+            if(GetComponent<AttackTypeClass>() != null){GetComponent<AttackTypeClass>().Attack();}
             return;
         }
 
         if(desired_coord.x > 0 && desired_coord.y > 0 && desired_coord.x < Levelplay_Controller_Script.levelplay_controller_singleton.x_lead_map_coord_array.Length && desired_coord.y < Levelplay_Controller_Script.levelplay_controller_singleton.x_lead_map_coord_array[desired_coord.x].Length && Levelplay_Controller_Script.levelplay_controller_singleton.x_lead_map_coord_array[desired_coord.x][desired_coord.y].resident == null)
         {
             move_timer = 0.10f;
-            playerAnimator.SetBool("Run", true);
+            enemyAnimator.SetBool("Run", true);
             Check_For_Exit_Tile(desired_coord);
             Levelplay_Controller_Script.levelplay_controller_singleton.x_lead_map_coord_array[grid_pos.x][grid_pos.y].resident = null;
             PlaceResidentTween(desired_coord,move_timer,false);

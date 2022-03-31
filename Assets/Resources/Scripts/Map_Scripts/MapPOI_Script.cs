ee9e16cd7c2eb91472a72fb48ca74ddccc51185b
Mitchell Greene <mitchell.lee.greene@gmail.com>
2022-03-30 21:21:56 -0400

    started work on the deco spawning

--- Assets/Resources/Scripts/Map_Scripts/MapPOI_Script.cs
+++ Assets/Resources/Scripts/Map_Scripts/MapPOI_Script.cs
@@ -8,13 +8,15 @@ public class MapPOI_Script : MonoBehaviour
     [SerializeField]private MeshRenderer poi_mesh_renderer;
     [SerializeField]private MeshFilter poi_mesh_filter;
     [SerializeField]private Mesh[] asteroid_meshs;
+    [SerializeField]private GameObject[] _decoMeshes;
+    [SerializeField]private float _decoDistFromCenter;
 
 
 
     public void Init(MapPOI_ScriptableObject new_poi_info_so)
     {
         poi_info_so = new_poi_info_so;
-        // Spawn_Deco();
+        Spawn_Deco();
         poi_mesh_filter.mesh = asteroid_meshs[new_poi_info_so.mesh_index];
         transform.position = new_poi_info_so.map_pos;
         transform.localScale *= new_poi_info_so.poi_size;
@@ -31,7 +33,7 @@ public class MapPOI_Script : MonoBehaviour
     {
         for (int i = 0; i < poi_info_so.deco_count; i++)
         {
-            
+            GameObject _newDeco = Instantiate(_decoMeshes[Global_Vars.rand_num_gen.Next(0, _decoMeshes.Length)], parent: this.transform);
         }
     }
 

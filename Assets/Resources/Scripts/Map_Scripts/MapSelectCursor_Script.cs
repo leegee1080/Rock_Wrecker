dd7cd283ca9e901edf40be66cc38592f4b48ad56
Mitchell Greene <mitchell.lee.greene@gmail.com>
2022-04-16 16:03:09 -0400

    added devised and implemented fuel system including art, finished drone deployment resource, added particles to map select screen space

--- Assets/Resources/Scripts/Map_Scripts/MapSelectCursor_Script.cs
+++ Assets/Resources/Scripts/Map_Scripts/MapSelectCursor_Script.cs
@@ -8,14 +8,38 @@ public class MapSelectCursor_Script : MonoBehaviour
     [SerializeField]TMP_Text _sizeText;
     [SerializeField]TMP_Text _lodeText;
     [SerializeField]TMP_Text _dangerText;
+    [SerializeField]TMP_Text _fuelText;
+    public bool FuelCostTooHigh;
     [SerializeField]Color _defaultColor;
     [SerializeField]Color _warnColor;
     [SerializeField]Color _alertColor;
+
+    private IEnumerator _runningBlinkRountine;
     public void UpdateInfo(MapPOI_ScriptableObject _selectedPOI)
     {
+        if(_runningBlinkRountine != null){StopCoroutine(_runningBlinkRountine); _fuelText.gameObject.transform.localScale = Vector3.one;}
+        
         _sizeText.text = "Size: "+_selectedPOI.poi_size;
         _lodeText.text = "Lode: "+_selectedPOI.poi_difficulty;
-        
+
+        _fuelText.fontSharedMaterial.SetColor(ShaderUtilities.ID_GlowColor, _defaultColor);
+        _fuelText.fontSharedMaterial.SetColor(ShaderUtilities.ID_FaceColor, _defaultColor);
+        int fuelCost = ((int)(Vector2.Distance(Levelselect_Controller_Script.levelselect_controller_singletion.OccupiedPOI.poi_info_so.map_pos, _selectedPOI.map_pos) / Levelselect_Controller_Script.levelselect_controller_singletion.DistPerFuelCost));
+        _fuelText.text = "Fuel: "+ fuelCost;
+        if(fuelCost > Overallgame_Controller_Script.overallgame_controller_singleton.PlayerFuel)
+        {
+           _fuelText.fontSharedMaterial.SetColor(ShaderUtilities.ID_GlowColor, _alertColor);
+           _fuelText.fontSharedMaterial.SetColor(ShaderUtilities.ID_FaceColor, _alertColor);
+           FuelCostTooHigh = true;
+        }
+        else
+        {
+           _fuelText.fontSharedMaterial.SetColor(ShaderUtilities.ID_GlowColor, _defaultColor);
+           _fuelText.fontSharedMaterial.SetColor(ShaderUtilities.ID_FaceColor, _defaultColor);
+           FuelCostTooHigh = false;
+        }
+
+
         switch (_selectedPOI.poi_difficulty)
         {
             case 0:
@@ -50,5 +74,23 @@ public class MapSelectCursor_Script : MonoBehaviour
                 break;
         }
 
+    }
+
+    public void StartBlinkCoroutine()
+    {
+        if(_runningBlinkRountine != null){StopCoroutine(_runningBlinkRountine);}
+        _runningBlinkRountine = BlinkFuelCost();
+        StartCoroutine(_runningBlinkRountine);
+    }
+
+    private IEnumerator BlinkFuelCost()
+    {  
+        for (int i = 0; i < 5; i++)
+        {
+            _fuelText.gameObject.transform.localScale = Vector3.zero;
+            yield return new WaitForSeconds(0.25f);
+            _fuelText.gameObject.transform.localScale = Vector3.one;
+            yield return new WaitForSeconds(0.25f);
+        }
     }
 }

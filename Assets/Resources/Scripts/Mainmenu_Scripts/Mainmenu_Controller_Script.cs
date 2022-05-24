5954a5e148618f38d75abca554ace5b5d22cb0fd
Mitchell Greene <mitchell.lee.greene@gmail.com>
2022-05-23 20:10:51 -0400

    work on the main menu \ buttons updated \ music added

--- Assets/Resources/Scripts/Mainmenu_Scripts/Mainmenu_Controller_Script.cs
+++ Assets/Resources/Scripts/Mainmenu_Scripts/Mainmenu_Controller_Script.cs
@@ -1,49 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-
-public enum Mainmenu_States_Enum
-{
-    home,
-    options,
-    credits,
-    quit,
-    play
-}
 
 public class Mainmenu_Controller_Script : MonoBehaviour
 {
     public static  Mainmenu_Controller_Script mainmenu_controller_singleton;
 
     [Header("Container Gameobjects")]
-    [SerializeField]
-    private GameObject home_container_go;
-    [SerializeField]
-    private GameObject options_container_go;
-    [SerializeField]
-    private GameObject credits_container_go;
-    [SerializeField]
-    private GameObject play_container_go;
-    [SerializeField]
-    private GameObject confirm_container_go;
+    [SerializeField]private GameObject home_container_go;
+    [SerializeField]private GameObject options_container_go;
+    [SerializeField]private GameObject credits_container_go;
+    [SerializeField]private GameObject play_container_go;
 
-
-    [Header("Overall Menu Vars")]
-    public Mainmenu_States_Enum mainmenu_state;
-
+    [Header("Ref Gameobjects")]
+    [SerializeField]private TMP_Text _playText;
 
 
     void Awake() => mainmenu_controller_singleton = this;
 
-    private void Start() {
+    private void Start()
+    {
         Playerinput_Controller_Script.playerinput_controller_singleton.camera_controls_allowed =false;
+        Playerinput_Controller_Script.playerinput_controller_singleton.on_screen_controls_allowed =false;
+
+        Sound_Events.Play_Sound("Music_Menu");
+        Sound_Events.Delay_Play_Sound("Music_MenuLoop",45f);
     }
 
-    public void New_Game(){
-        // Overallgame_Controller_Script.overallgame_controller_singleton.Create_Map();
-        // Loading_Controller_Script.loading_controller_singleton.Load_Next_Scene(Scene_Enums.levelselect);
-
+    public void New_Game()
+    {
+        Sound_Events.Stop_Sound("Music_Menu");
+        Sound_Events.Stop_Sound("Music_MenuLoop");
+        Audio_Controller_Script.singleton.StopAllCoroutines();
         ScnTrans_Script.singleton.ScnTransOut(Scene_Enums.levelselect);
+    }
+
+    public void NewGame()
+    {
+
+    }
+
+    public void ContinueGame()
+    {
+
+    }
+
+    public void ShowOptions()
+    {
+
+    }
+    public void QuitGame()
+    {
+
     }
 }
